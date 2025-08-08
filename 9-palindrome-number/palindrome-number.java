38450ca66bcf721958a5bb238678f9437d4e8304
Mohammed Samiq Khan <145823604+Samiqkhan@users.noreply.github.com>
2025-08-08 09:23:26 +0530

    Time: 6 ms (26.58%) | Memory: 43.9 MB (88.81%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
+        if(x<0)
+        {
+            return false;
+        }
         String s  = String.valueOf(x);
         int l = 0;
         int r = s.length() -1;
