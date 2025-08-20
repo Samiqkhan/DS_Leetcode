f27db27efa788abf952421baab23ec81f9ca7930
Mohammed Samiq Khan <145823604+Samiqkhan@users.noreply.github.com>
2025-08-20 18:09:51 +0530

    Time: 2 ms (96.56%) | Memory: 41.9 MB (31.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
         int n = s.length();
-        Stack <Character> stack = new Stack<>();
-        for(char a: s.toCharArray())
+        Stack<Character> stack = new Stack<>();
+        for(char a : s.toCharArray())
         {
             if(a == '[')
+            {
                 stack.push(']');
+            }
             else if(a == '{')
+            {
                 stack.push('}');
+            }
             else if(a == '(')
+            {
                 stack.push(')');
+            }
             else if(stack.isEmpty() || stack.pop() != a)
+            {
                 return false;
+            }
         }
         return stack.isEmpty();
-
     }
 }
